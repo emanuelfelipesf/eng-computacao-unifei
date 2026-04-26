0ff4063a76d22481a7399f8901f5743ac920fb21
emanuelfsf <d2026011307@unifei.edu.br>
2026-04-26 13:21:33 -0300

    update lista 5

--- lista_5.cpp
+++ lista_5.cpp
@@ -2,7 +2,6 @@
 #include <cmath>
 using namespace std;
 
-
 void numeroImparFor();
 void numeroImparWhile();
 void numeroImparDoWhile();
@@ -23,7 +22,7 @@ void questao2()
 
     for (int i = 0; i < 10; i++)
     {
-        cout << "Digite a Idade da pessoa " << i+1 << ": ";
+        cout << "Digite a Idade da pessoa " << i + 1 << ": ";
         cin >> idades[i];
     }
     cout << "A quantidade de pessoas com menos de 18 anos e: " << quantidadeMaiorIdade(idades);
@@ -89,14 +88,14 @@ void questao7()
     int numeros[1000];
     int n = 0;
     int valor;
-    
+
     cout << "Digite valores inteiros e positivos (0 para encerrar):\n";
-    
+
     while (true)
     {
         cout << "\nValor: ";
         cin >> valor;
-        
+
         if (valor == 0)
         {
             break;
@@ -111,7 +110,7 @@ void questao7()
             n++;
         }
     }
-    
+
     if (n == 0)
     {
         cout << "Nenhum valor valido foi digitado.\n";
@@ -125,114 +124,132 @@ void questao7()
     }
 }
 
-
+float potencia(int x, int y);
 void questao8()
 {
-    
+    int x, y;
+
+    cout << "Digite o valor de x: ";
+    cin >> x;
+    cout << "Digite o valor de y: ";
+    cin >> y;
+
+    cout << "O valor de x e: " << potencia(x, y);
 }
 
-
+float mediaIdades(int idades, int quantidade);
+float mediaHomens(int idades, int quantidade);
+float mediaMulheres(int idades, int quantidade);
 void questao9()
 {
+    bool loop = true;
+    int idade, qntd_f = 1, qntd_m = 1, qntd_total = 1, soma_idade = 0;
+    char sexo;
+
+    int idades[qntd_total], sexos_m[qntd_m], sexos_f[qntd_f];
+
+    while (loop);
+    {
+        cout << "Digite a idade: ";
+        cin >> idade;
+        cout << "Digite o sexo: ";
+        cin >> sexo;
+
+        if (idade > 0)
+        {
+            qntd_total++;
+            soma_idade += idade;
+            idades[qntd_total - 1] = idade;
+        }
+        else
+        {
+            loop = false;
+        }
+        
+        if (sexo == 'f')
+        {
+            qntd_f++;
+        }
+        else if (sexo == 'm')
+        {
+            qntd_m++;
+        }
+        else
+        {
+            loop = false;
+        }
+    }
+
+    for (int i = 0; i < qntd_total; i++)
+    {
+        cout << idades[i] << endl;
+    }
     
 }
-
 
 void questao10()
 {
-    
 }
-
 
 void questao11()
 {
-    
 }
-
 
 void questao12()
 {
-    
 }
-
 
 void questao13()
 {
-    
 }
-
 
 void questao14()
 {
-    
 }
-
 
 void questao15()
 {
-    
 }
-
 
 void questao16()
 {
-    
 }
-
 
 void questao17()
 {
-    
 }
-
 
 void questao18()
 {
-    
 }
-
 
 void questao19()
 {
-    
 }
-
 
 void questao20()
 {
-    
 }
-
 
 void questao21()
 {
-    
 }
-
 
 void questao22()
 {
-    
 }
-
 
 void questao23()
 {
-    
 }
-
 
 void questao24()
 {
-    
 }
-
 
 void questao25()
 {
-    
 }
-
 
 int main()
 {
@@ -398,7 +415,7 @@ void sequenciaE(int n, double &e)
 {
     for (int i = 0; i <= n; i++)
     {
-        e += 1/(tgamma(i + 1));
+        e += 1 / (tgamma(i + 1));
     }
 }
 int fatorial(int n)
@@ -416,7 +433,7 @@ void maiorMenorConjunto(int numeros[], int n, int &maior, int &menor)
 {
     maior = numeros[0];
     menor = numeros[0];
-    
+
     for (int i = 1; i < n; i++)
     {
         if (numeros[i] > maior)
@@ -428,4 +445,32 @@ void maiorMenorConjunto(int numeros[], int n, int &maior, int &menor)
             menor = numeros[i];
         }
     }
+}
+float potencia(int x, int y)
+{
+    int aux = x;
+
+    if (y == 0)
+    {
+        return 1;
+    }
+
+    while (y != 1)
+    {
+        aux *= x;
+        y--;
+    }
+    return aux;
+}
+float mediaIdades(int idades, int quantidade)
+{
+    return idades/quantidade;
+}
+float mediaHomens(int idades, int quantidade)
+{
+    return idades/quantidade;
+}
+float mediaMulheres(int idades, int quantidade)
+{
+    return idades/quantidade;
 }
