613f2680a286dbdc34d5b5e0afdd4180ec2c3d17
emanuelfsf <d2026011307@unifei.edu.br>
2026-04-30 15:15:54 -0300

    exeercri

---
+++ exercicios/exercicio02.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+// float expressao(int n, int X);
+    // int main()
+    // {
+    //     int n, X;
+    //     cin >> n >> X;
+    //     cout << expressao(n, X);
+    //     return 0;
+    // }
+    // float expressao(int n, int X)
+    // {
+    //     int divisores[4] = {-1, 2, -3, 4}, invertida[4] = {4, -3, 2, -1}, aux = 0;
+    //     float soma = 0;
+    //     for (int i = 0; i < n; i++)
+    //     {
+    //         if (condition)
+    //         {
+    //             /* code */
+    //         }
+    //         soma += (i + 1)*X/divisores[aux];
+    //         cout << soma << "  "<< i + 1 << "*" << X << "/" << divisores[aux] << endl;
+    //         aux++;
+    //     }
+    //     return soma;
+    // }
+
+int main(int argc, char const *argv[])
+{
+    int vetor[15], vetor2[15], n = 15;
+
+
+    return 0;
+}
+
+float produtoEscalar(float vetor[], int n, float vetor2[], int m)
+{
+    float produto = 0, angulo;
+
+    for (int i = 0; i < n; i++)
+    {
+        produto += vetor[i] * vetor2[i];
+    }
+    
+    
+    
+    return angulo;
+}
