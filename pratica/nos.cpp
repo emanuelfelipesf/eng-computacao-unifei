30f8f21f5731abe7cf2aa06e3122186d72a679ba
emanuelfelipesf <d2026011307@unifei.edu.br>
2026-09-24 20:38:49 -0300

    Implement template class 'No' for linked list nodes
    Added a template class 'No' with constructors and a method to get the next node.

--- pratica/nos.cpp
+++ pratica/nos.cpp
@@ -1,1 +1,18 @@
+template <class TipoItem>
 
+class No {
+  private:
+    No *prox;
+    TipoItem item;
+  public:
+    No(){
+      prox = NULL;
+    }
+    No(TipoItem x){
+      item = x;
+      prox = NULL;
+    }
+
+    No* getProx(){
+      return prox;
+    }
